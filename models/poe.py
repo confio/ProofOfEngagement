ee1fd063b3ee1491c700253bb513bf7ed6386ffd
willclarktech <willclarktech@users.noreply.github.com>
2020-09-30 17:40:08 +0200

    Use classes for validators/delegators

--- models/poe.py
+++ models/poe.py
@@ -7,38 +7,27 @@ def default_sigmoidal(stake, engagement):
 
 def calculate_voting_weight(validator, params):
     f = params['f']
-    self_stake = validator['self_stake']
-    delegation = validator['delegation']
-    engagement = validator['engagement']
-
-    return f(self_stake + delegation, engagement)
+    return f(validator.self_stake + validator.delegation, validator.engagement)
 
 
 def calculate_reward(validator, total_voting_weight, params):
     inflation_rate = params['inflation_rate']
     f = params['f']
-    self_stake = validator['self_stake']
-    engagement = validator['engagement']
-    commission_rate = validator['commission_rate']
 
     voting_weight = calculate_voting_weight(validator, params)
-    self_stake_voting_weight = f(self_stake, engagement)
+    self_stake_voting_weight = f(validator.self_stake, validator.engagement)
 
-    return inflation_rate * (self_stake_voting_weight + commission_rate * (voting_weight - self_stake_voting_weight)) / total_voting_weight
+    return inflation_rate * (self_stake_voting_weight + validator.commission_rate * (voting_weight - self_stake_voting_weight)) / total_voting_weight
 
 
 def calculate_yield(validator, total_voting_weight, params):
     inflation_rate = params['inflation_rate']
     f = params['f']
-    commission_rate = validator['commission_rate']
-    self_stake = validator['self_stake']
-    engagement = validator['engagement']
-    delegation = validator['delegation']
 
-    if delegation == 0:
+    if validator.delegation == 0:
         return 0
 
     voting_weight = calculate_voting_weight(validator, params)
-    self_stake_voting_weight = f(self_stake, engagement)
+    self_stake_voting_weight = f(validator.self_stake, validator.engagement)
 
-    return (inflation_rate * (1 - commission_rate) * (voting_weight - self_stake_voting_weight)) / (delegation * total_voting_weight)
+    return (inflation_rate * (1 - validator.commission_rate) * (voting_weight - self_stake_voting_weight)) / (validator.delegation * total_voting_weight)
